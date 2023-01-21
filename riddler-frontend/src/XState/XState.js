e4deaa110158e440ede34e78e1ad475313fceb8c
Crystal R. Williams <cortney0215@gmail.com>
2023-01-21 13:53:00 -0500

    add state machine

--- riddler-frontend/src/XState/XState.js
+++ riddler-frontend/src/XState/XState.js
@@ -1,12 +1,55 @@
 import { createMachine, assign } from 'xstate';
 
 const promiseMachine = createMachine({
-  id: 'puzzle',
-  initial: 'idle',
-  states: {
-    idle: {},
-    selected: {},
-  },
-});
+    id: "Machine Name",
+    initial: "upload_puzzle",
+    states: {
+      upload_puzzle: {
+        on: {
+          picture_is_loaded: {
+            target: "upload_puzzle_piece",
+          },
+        },
+      },
+      upload_puzzle_piece: {
+        on: {
+          piece_is_loaded: {
+            target: "upload_to_backend",
+          },
+        },
+      },
+      upload_to_backend: {
+        on: {
+          waiting_for_response: [
+            {
+              target: "display_solution",
+              cond: "success",
+            },
+            {
+              target: "error",
+              cond: "failure",
+            },
+          ],
+        },
+      },
+      display_solution: {
+        on: {
+          new_puzzle: {
+            target: "upload_puzzle",
+          },
+        },
+      },
+      error: {
+        on: {
+          new_puzzle: {
+            target: "upload_puzzle",
+          },
+        },
+      },
+    },
+    context: {},
+    predictableActionArguments: true,
+    preserveActionOrder: true,
+  });
 
 export default promiseMachine;
