3fbedec02a6ac8b8c1d1f0721df387063b5ebec3
itsfloppyhat <itsfloppyhat@gmail.com>
2023-01-21 11:07:12 -0500

    added box and state machine

--- riddler-frontend/src/Frame.js
+++ riddler-frontend/src/Frame.js
@@ -1,9 +1,20 @@
 import React from "react"
+import { Box } from '@mui/material'
 
 const Frame = () => {
     return (
-        <div>Frame </div>
-    )
+        <Box
+          sx={{
+            width: 750,
+            height: 550,
+            backgroundColor: 'primary.dark',
+            '&:hover': {
+              backgroundColor: 'primary.main',
+              opacity: [0.9, 0.8, 0.7],
+            },
+          }}
+        />
+      );
 }
 
 export default Frame
